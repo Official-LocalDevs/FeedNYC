d0a7f10233f9e46b170b53659589085a3e7fd935
Mohammad Haider <arzhdr@gmail.com>
2024-03-06 13:32:50 -0500

    connected to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,18 @@
+//needed modules
 const express = require("express");
-const { MongoClient, ObjectId } = require("mongodb");
-const url = "";
-const db = "";
-const client = newMongoClient(url);
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
+//import models
+const Hours = require("./models/Hour");
+const Place = require("./models/Place");
+
+const app = express();
+// parse JSON requests
+app.use(express.json());
+
+//connecting to mongo
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
