d711b3e75e3a8e4011782fdc902935ef8c76397f
Mohammad Haider <arzhdr@gmail.com>
2024-03-31 17:00:47 -0400

    Removed Description

--- backend/models/Place.js
+++ backend/models/Place.js
@@ -1,42 +1,39 @@
-const mongoose = require('mongoose')
-const hourSchema = require('./Hour.js')
+const mongoose = require("mongoose");
+const hourSchema = require("./Hour.js");
 
 const placeSchema = new mongoose.Schema({
-    name: { 
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String, 
-        unique: true,
-        required: true,
-    },
-    type: {
-        type: String,
-        enum: ['Food Pantry', 'Soup Kitchen', 'Food Bank', 'Free Groceries'],
-        required: true,
-    },
-    borough: {
-        type:String, 
-        enum: ['Manhattan', 'Brooklyn', 'Queens', 'Bronx', 'Staten Island'],
-        required: true,
-    },
-    tags: {
-        type:[String],
-        enum:['Halal', 'Vegetarian','Kosher']
-    },
-    description: { 
-        type:String, 
-        maxLength: 500 
-    },
-    contact: {
-        type: String,
-        required: true
-    },
-    hours: {
-        type:hourSchema,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["Food Pantry", "Soup Kitchen", "Food Bank", "Free Groceries"],
+    required: true,
+  },
+  borough: {
+    type: String,
+    enum: ["Manhattan", "Brooklyn", "Queens", "Bronx", "Staten Island"],
+    required: true,
+  },
+  tags: {
+    type: [String],
+    enum: ["Halal", "Vegetarian", "Kosher"],
+  },
 
-module.exports = mongoose.model('places', placeSchema)
+  contact: {
+    type: String,
+    required: true,
+  },
+  hours: {
+    type: hourSchema,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("places", placeSchema);
