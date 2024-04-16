9dfb94d48c39b081701ea7009bf0f3cf47385e83
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2024-04-16 15:53:27 -0400

    Revert "added connection to mongo as per ticket: Once PROD db is made, change URI in NodeJS for successful connection"
    This reverts commit 8281a20119eb7ee84faffaaabc81352729da2685.

--- backend/controllers/placeController.js
+++ backend/controllers/placeController.js
@@ -4,36 +4,36 @@ let Place = require('../models/Place');
 
 //Endpoint to getall
 const getPlaces = async (req, res) => {
-  try {
-    const places = await Place.find();
-    res.json(places);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
-
-//Endpoint to get place by ID
-const getPlaceById = async (req, res) => {
-  try {
-    const place = await Place.findById(req.params.id);
-    if (place) {
-      res.json(place);
-    } else {
-      res.status(404).send("Place not found");
+    try {
+      const places = await Place.find();
+      res.json(places);
+    } catch (err) {
+      res.status(500).send(err.message);
     }
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
+  };
+  
+  //Endpoint to get place by ID
+  const getPlaceById = async (req, res) => {
+    try {
+      const place = await Place.findById(req.params.id);
+      if (place) {
+        res.json(place);
+      } else {
+        res.status(404).send("Place not found");
+      }
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  };
+  
+  //Endpoint to get place by borough
+  const getPlacesByBorough = async (req, res) => {
+    try {
+      const places = await Place.find({ borough: req.params.borough });
+      res.json(places);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  };
 
-//Endpoint to get place by borough
-const getPlacesByBorough = async (req, res) => {
-  try {
-    const places = await Place.find({ borough: req.params.borough });
-    res.json(places);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
-};
-
-module.exports = { getPlaces, getPlaceById, getPlacesByBorough };
+module.exports = {getPlaces, getPlaceById, getPlacesByBorough};
--- backend/routes/placeRoutes.js
+++ backend/routes/placeRoutes.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const placeController = require('../controllers/placeController');
 
 //Endpoint to getall
-router.get("/getAll", placeController.getPlaces);
-
+router.get("/", placeController.getPlaces);
+  
 //Endpoint to get place by ID
 router.get("/:id", placeController.getPlaceById);
-
+  
 //Endpoint to get place by borough
 router.get("/borough/:borough", placeController.getPlacesByBorough);
 
